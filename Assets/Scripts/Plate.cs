e10917db54a2d2586dbcfe2baf06d1e023dc6f4d
Nikita <bednyakov99@gmail.com>
2021-02-16 19:41:13 +0300

    initial commit

---
+++ Assets/Scripts/Plate.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    #region Fields
+
+    #endregion
+
+    #region Properties
+
+    public static Vector2 PositionForPlayerMove { get; private set; }
+
+    public static bool MoveIsCorrect { get; private set; }
+
+    public static Vector2 PlatePosition { get; private set; }
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        PlatePosition = transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveIsCorrect = false;
+    }
+
+    private void OnMouseDown()
+    {
+        PositionForPlayerMove = transform.position;
+
+        if (Mathf.Abs(transform.position.x - Player.PlayerPosition.x) < 1.4f
+            && Mathf.Abs(transform.position.y - Player.PlayerPosition.y) <= 0.01f
+            || Mathf.Abs(transform.position.x - Player.PlayerPosition.x) <= 0.01f
+            && Mathf.Abs(transform.position.y - Player.PlayerPosition.y) < 1.4)
+            MoveIsCorrect = true;
+    }
+    #endregion
+
+}
