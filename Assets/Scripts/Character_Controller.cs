0d9aea3e956e638aef4fe3fbc9ea0d049fcb53d2
Nikita <bednyakov99@gmail.com>
2021-02-16 19:52:05 +0300

    initial commit

---
+++ Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] Transform movePoint;
+    [SerializeField] LayerMask noMovementLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        
+        if (Vector3.Distance(transform.position, movePoint.position) == 0)
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            
+            if (Mathf.Abs(horizontalInput) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalInput, 0f, 0f), 0.1f, noMovementLayer))
+                    movePoint.position += new Vector3(horizontalInput, 0f, 0f);
+                
+            }
+            else if (Mathf.Abs(verticalInput) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalInput, 0f), 0.2f, noMovementLayer))
+                    movePoint.position += new Vector3(0f, verticalInput, 0f);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(16, 7, 1f, new Vector3(-8, -3));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+            grid.SetValue(mousePosition, 1);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+            Debug.Log(grid.GetValue(mousePosition));
+        }
+    }
+
+}
